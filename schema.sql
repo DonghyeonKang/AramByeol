7e9cb1c773df13d37c572091a0c0f46a2fba2dd9
Donghyeon Kang <donghyeon009@gmail.com>
2021-11-07 15:28:29 +0900

    Update schema.sql

---
+++ schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE `menuData`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `menu` TEXT,
+    `star` INT
+);
+
+CREATE TABLE `dinner`(
+    `day` TEXT NOT NULL,
+    `cource` TEXT,
+    `menu` TEXT NOT NULL
+);
+
+CREATE TABLE `lunch`(
+    `day` TEXT NOT NULL,
+    `cource` TEXT,
+    `menu` TEXT NOT NULL
+
+);
+
+CREATE TABLE `morning`(
+    `day` TEXT NOT NULL,
+    `cource` TEXT,
+    `menu` TEXT NOT NULL
+);
+
+CREATE TABLE `week`(
+    `day` TEXT NOT NULL,
+    `date` DATE NOT NULL
+);
+
+CREATE TABLE `user` (
+    `id` TEXT NOT NULL,
+    `password` TEXT NOT NULL,
+);
+
+CREATE TABLE `review` (
+    `identify` TEXT NOT NULL,
+    `menu` TEXT NOT NULL,
+    `star` INT NOT NULL
+);
