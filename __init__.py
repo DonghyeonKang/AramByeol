4a7167b3f02fcbd8b350873470914b92ee157c13
Donghyeon Kang <86303312+DonghyeonKang@users.noreply.github.com>
2021-11-14 19:00:21 +0900

    Update login, register function
    login, register 함수 업데이트. 하나로 축소함

--- __init__.py
+++ __init__.py
@@ -16,35 +16,28 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/member/register.html')
+@app.route('/member/register.html', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        id = request.form['id']
+        pw = request.form['password']
+        pw = (bcrypt.hashpw(pw.encode('UTF-8'), bcrypt.gensalt())).decode('utf-8')  # 해싱 처리
+        if(check_userId(id) != True):   # id 가 이미 존재하면 true
+            useradd(id, pw)
+        else:
+            print("이미 존재하는 아이디")
+        return redirect('http://localhost:5001/member/login.html')
     return render_template("/member/register.html")
 
-@app.route('/member/login.html')
+@app.route('/member/login.html', methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        id = request.form['id']
+        pw = request.form['password']
+        account, check_password = login_check(id, pw)
+        print(account, check_password)
+        return render_template("./index.html")
     return render_template("/member/login.html")
-
-# register request
-@app.route('/registerr', methods=['GET', 'POST'])
-def registerr():
-    id = request.form['id']
-    pw = request.form['password']
-    pw = (bcrypt.hashpw(pw.encode('UTF-8'), bcrypt.gensalt())).decode('utf-8')  # 해싱 처리
-    if(check_userId(id) != True):   # id 가 이미 존재하면 true
-        useradd(id, pw)
-    else:
-        print("이미 존재하는 아이디")
-    return redirect('http://localhost:5001/member/login.html')
-
-# login request
-@app.route('/loginn', methods=['GET', 'POST'])
-def loginn():
-    id = request.form['id']
-    pw = request.form['password']
-
-    account, check_password = login_check(id, pw)
-    print(account, check_password)
-    return render_template("./index.html")
 
 # review request
 if __name__ == "__main__":
