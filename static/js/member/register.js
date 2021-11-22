31795cf10c1c2f969e926694d4cd3096bdfce259
JEONJEYUN <91324720+JEONJEYUN@users.noreply.github.com>
2021-11-22 20:26:12 +0900

    Finish project
    제윤의 분량을 마무리하다.

--- static/js/member/register.js
+++ static/js/member/register.js
@@ -1,32 +1,32 @@
-function regPasswordType(p1) {
-  var regex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{6,16}/;
-  return regex.test(p1);
+function idchk(id) {
+  let id_chk = /^[A-za-z0-9]{5,15}/g;
+  return id_chk.test(id);
 }
 
-function idchk(p0) {
-  var idReg = /^[A-za-z0-9]{5,15}/g;
-  return idReg.test(p0);
+function password_chk(pwd) {
+  let pwd_chk = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{6,16}/;
+  return pwd_chk.test(pwd);
 }
 
-function test() {
-  const p0 = document.getElementById("nameInput").value;
-  const p1 = document.getElementById("pwdInput").value;
-  const p2 = document.getElementById("pwdInput_chk").value;
-  const signupbtn = document.querySelector(".signup_btn");
+function playsignup() {
+  const id = document.getElementById("nameInput").value;
+  const pwd = document.getElementById("pwdInput").value;
+  const pwdchk = document.getElementById("pwdInput_chk").value;
+  const pwd_chkbtn = document.querySelector(".pwdchk_button");
   const signup_btn = document.querySelector(".signup_button");
 
-  if (idchk(p0)) {
-    if (regPasswordType(p1)) {
-      if (p1 == p2) {
-        signupbtn.style.display = "block";
-        signup_btn.style.display = "none";
+  if (idchk(id)) {
+    if (password_chk(pwd)) {
+      if (pwd == pwdchk) {
+        pwd_chkbtn.style.display = "none";
+        signup_btn.style.display = "block";
       } else {
         alert("비밀번호가 일치하지 않습니다.");
       }
     } else {
-      alert("비밀번호가 정규식에 맞지 않습니다.");
+      alert("비밀번호 형식이 올바르지 않습니다.");
     }
   } else {
-    alert("아이디가 정규식에 맞지 않습니다.");
+    alert("아이디 형식이 올바르지 않습니다.");
   }
 }
