c2d5c47c791c41ef107e795a6f2c6136913d2b1f
DonghyeonKang <donghyeon009@gmail.com>
2022-02-01 18:45:28 +0900

    Fix blank in menu

--- get_data.py
+++ get_data.py
@@ -20,18 +20,15 @@ driver.close()
 display.stop()
 subprocess.call("pkill -9 chrome", shell=True) # chrome driver 제대로 안꺼지면 꺼야함
 soup = BeautifulSoup(html, 'html.parser') # Beautifulsoup로 1차 가공
-
 #br태그를 공백문자로 바꾸어 준다.
 for elem in soup.find_all(["br"]):
     elem.append('\n')
 
-table = soup('#detailForm > div > table') # 1차적으로 식단표 전체를 스크래핑
 # days : 날짜 및 요일, morning : 아침, lunch : 점심, dinner : 저녁
-days = soup.select_one('#detailForm > div > table > thead > tr').text.strip().replace(" ", "").split('\n')
+days = soup.select_one('#detailForm > div > table > thead > tr').text.strip().replace(" ", "").split('\n')      # NoneType 이면 IP 차단 되었을 가능성 있음
 morning = soup.select_one('#detailForm > div > table > tbody > tr:nth-child(1)').text.strip().split('\n')
 lunch = soup.select_one('#detailForm > div > table > tbody > tr:nth-child(2)').text.strip().split('\n')
 dinner = soup.select_one('#detailForm > div > table > tbody > tr:nth-child(3)').text.strip().split('\n')
-
 def first_index_del(arg, repeat=1):   # 첫번째 인덱스를 삭제. ('아침', '점심', '저녁')
         for i in range(repeat):
             del arg[0]
@@ -62,21 +59,25 @@ def split_menu_data(args):
         day_count = 0   # 요일 카운트 [요일][메뉴]
         menu_count = 0  # 메뉴 카운트
 
-        print(args)
 
         for element in args:
+
+            if count >= 4 and element != '':  # 공백 개수가 연속으로 4이고, 5번째가 공백이 아니라면
+                day.append([])  # 요일 바뀜
+                day_count += 1  # 요일 바꾸기
+                count = 0   # 공백 개수 초기화
+            elif count >= 5 and element == '':
+                day.append([])  # 요일 바뀜
+                day_count += 1  # 요일 바꾸기
+                count = 0   # 공백 개수 초기화
+        
             if element == '': # 공백 인덱스면
                 count += 1  # 공백 개수를 세고
-                continue    # 건너 뛰어라
-            if count >= 4 :  # 공백 개수가 연속으로 5와 같거나 크면
-                if not(element == "B" or element == "C" or element == "테이크아웃"): # 같은 요일의 바로 다음 코스인데 공백 개수가 5보다 클 때
-                    day.append([])  # 요일 바뀜
-                    day_count += 1  # 요일 바꾸기
-            count = 0   # 공백 개수 초기화
-            day[day_count].append(element)
+            else:
+                day[day_count].append(element)
+                count = 0   # 공백 개수 초기화
         return day
 
 day_mornings = split_menu_data(morning)
 day_lunchs = split_menu_data(lunch)
 day_dinners = split_menu_data(dinner)
-
