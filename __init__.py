3ade072debcce7782782e372ceefb8967f595bea
Develop <donghyeon009@gmail.com>
2022-02-27 18:40:14 +0000

    Fix indent

--- __init__.py
+++ __init__.py
@@ -243,4 +243,4 @@ def get_score():
     return jsonify({'score':score})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,debug=True, threaded=True)
+    app.run('0.0.0.0',port=5000,debug=False, threaded=True)
